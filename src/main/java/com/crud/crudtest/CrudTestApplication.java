7ead4bb56ee59090c32b019782b5e6997a5c11fc
Ademilson Silva <ademilson.w6@hotmail.com>
2022-07-11 19:02:45 -0300

    .;

--- src/main/java/com/crud/crudtest/CrudTestApplication.java
+++ src/main/java/com/crud/crudtest/CrudTestApplication.java
@@ -3,28 +3,11 @@ package com.crud.crudtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 
 @SpringBootApplication
 public class CrudTestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CrudTestApplication.class, args);
-
-
-
-        LocalDate localDate = LocalDate.now();
-        System.out.println(localDate);
-        //converter uma LocalDate para LocalDateTime no formato yyyy-MM-dd HH:mm:ss
-        LocalDateTime localDateTime = localDate.atStartOfDay();
-        System.out.println(localDateTime);
-
-
-
-
-
 
     }
 }
